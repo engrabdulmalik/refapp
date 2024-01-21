0d6ddcca895fe822f6d4d6b630b992ec332836dc
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-01-21 13:48:11 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-
+import { useRef } from "react";
 function App() {
-  return (
-    <div >
-      <form on>
+  const username = useRef();
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const myName = username.current.value;
+    console.log(myName);
+    username.current.value = "";
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>Name</label>
+        <input type="text" ref={username} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
